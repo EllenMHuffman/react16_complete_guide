be4397acf7f51ccc030f22f8aed83b7d504c1e79
Ellen Huffman <ellen@eventbrite.com>
2018-06-18 17:48:28 -0700

    fetch ingredients from backend

--- burger_builder/src/hoc/withErrorHandler/withErrorHandler.js
+++ burger_builder/src/hoc/withErrorHandler/withErrorHandler.js
@@ -10,7 +10,7 @@ const withErrorHandler = (WrappedComponent) => {
             error: null
         };
 
-        componentDidMount () {
+        componentWillMount () {
             axios.interceptors.request.use(req => {
                 this.setState({error: null});
                 return req;
